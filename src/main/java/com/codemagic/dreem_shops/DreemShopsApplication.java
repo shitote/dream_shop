57c0cfef5c6bb05fa999eb07ece0e00832ed5111
shitote <steveshitote00@gmail.com>
2026-05-09 17:52:04 +0300

    data models

---
+++ src/main/java/com/codemagic/dreem_shops/DreemShopsApplication.java
@@ -0,0 +1,13 @@
+package com.codemagic.dreem_shops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreemShopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreemShopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codemagic/dreem_shops/DreemShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemagic.dreem_shops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreemShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
